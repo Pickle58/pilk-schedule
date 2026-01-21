46c80840cc47d7194fc60cd86ae105d10d799423
James Pilkington <jpilkington332@gmail.com>
2026-01-21 07:07:57 -0700

    feat: finished branch

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
 import { type Metadata } from 'next'
-import { ClerkProvider } from '@clerk/nextjs'
 import { Geist, Geist_Mono } from 'next/font/google'
+import { ClerkProvider } from '@clerk/nextjs'
+import { SanityLive } from '@/sanity/lib/live'
 import './globals.css'
+import { TimezoneDetector } from '@/components/timezone-detector'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -27,8 +29,11 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-          
-          {children}
+          <main>
+            {children}
+          </main>
+          <SanityLive />
+          <TimezoneDetector />
         </body>
       </html>
     </ClerkProvider>
